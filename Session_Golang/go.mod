ee76d23a3f288c994100c11ebd195b366765071d
ejakpratama <muhammad.pratama@multipolar.com>
2023-01-20 22:28:50 +0700

    session golang done

---
+++ Session_Golang/go.mod
@@ -0,0 +1,20 @@
+module session_golang
+
+go 1.19
+
+require (
+	github.com/antonlindstrom/pgstore v0.0.0-20220421113606-e3a6e3fed12a // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/lib/pq v1.10.5 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+)
---
+++ Session_Golang/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/antonlindstrom/pgstore"
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo"
+)
+
+const SESSION_ID = "test-session-id"
+
+func newPostgresStore() *pgstore.PGStore {
+	url := "postgres://postgres:postgresss@127.0.0.1:5432/golang_session?sslmode=disable"
+	authKey := []byte("my-auth-key-very-secret")
+	encryptionKey := []byte("my-encryption-key-very-secret123")
+
+	store, err := pgstore.NewPGStore(url, authKey, encryptionKey)
+	if err != nil {
+		log.Println("ERROR", err)
+		os.Exit(0)
+	}
+
+	return store
+}
+
+func newCookieStore() *sessions.CookieStore {
+	authKey := []byte("my-auth-key-very-secret")
+	encryptionKey := []byte("my-encryption-key-very-secret123")
+
+	store := sessions.NewCookieStore(authKey, encryptionKey)
+	store.Options.Path = "/"
+	store.Options.MaxAge = 86400 * 7
+	store.Options.HttpOnly = true
+
+	return store
+}
+
+func main() {
+	e := echo.New()
+	store := newPostgresStore()
+
+	e.GET("/", func(ctx echo.Context) error {
+		data := "Hello from /index"
+		return ctx.String(http.StatusOK, data)
+	})
+
+	e.GET("/set", func(c echo.Context) error {
+		session, err := store.Get(c.Request(), SESSION_ID)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+		session.Values["message1"] = "This is hello"
+		session.Values["message2"] = "world session"
+		if err := session.Save(c.Request(), c.Response()); err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		return c.Redirect(http.StatusTemporaryRedirect, "/get")
+	})
+
+	e.GET("/get", func(c echo.Context) error {
+		session, _ := store.Get(c.Request(), SESSION_ID)
+
+		if len(session.Values) == 0 {
+			return c.String(http.StatusOK, "empty result")
+		}
+
+		return c.String(http.StatusOK, fmt.Sprintf(
+			"%s %s",
+			session.Values["message1"],
+			session.Values["message2"],
+		))
+	})
+
+	e.GET("/delete", func(c echo.Context) error {
+		session, _ := store.Get(c.Request(), SESSION_ID)
+		session.Options.MaxAge = -1
+		session.Save(c.Request(), c.Response())
+
+		return c.Redirect(http.StatusTemporaryRedirect, "/get")
+	})
+
+	e.Start(":9000")
+}
